9ae38a7ec6fa6f299399a39d4d9b548e6aeca525
BottoAhmet <bottosidi@gmail.com>
2026-02-10 20:54:40 +0100

    Update import paths for UtilisateurService and verify_password

--- backend/src/auth/aut_handler.py
+++ backend/src/auth/aut_handler.py
@@ -14,8 +14,8 @@ from typing import Optional
 from jose import jwt, JWTError
 from dotenv import load_dotenv
 
-from backend.utilisateur_service import UtilisateurService
-from backend.security import verify_password
+from backend.business_object.user_service import UtilisateurService
+from backend.utils.security import verify_password
 
 # Charger les variables d'environnement
 load_dotenv()
